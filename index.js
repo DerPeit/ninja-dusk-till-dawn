8473f548af0a2925af41101aa931371f88f4a1b1
Manuel Josupeit-Walter <mail@josupeit.com>
2014-02-19 20:12:24 +0100

    Reimplemented device entirely event based; Introduced node-cron

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var Device = require('./lib/device')
-  , util = require('util')
+var configHandlers = require('./lib/config-handlers')
+  , Device = require('./lib/device')
+  , EventBus = require('events').EventEmitter
   , stream = require('stream')
-  , configHandlers = require('./lib/config-handlers');
+  , UpdateObservable = require('./lib/update-observable')
+  , util = require('util')
+  ;
 
 // Give our driver a stream interface
 util.inherits(DuskTillDawn, stream);
@@ -27,35 +30,41 @@ function DuskTillDawn(opts, app) {
   this._app = app;
   this._opts = opts;
 
+  this.eventbus = new EventBus();
+  
   var self = this;
+  var locations = opts.locations || [];
+  var updateObservable = new UpdateObservable(this.eventbus, app.log);
+  var device = new Device(this.eventbus, app.log, locations);
+
+  /**
+   * Called when a user prompts a configuration.
+   * If `rpc` is null, the user is asking for a menu of actions
+   * This menu should have rpc_methods attached to them
+   *
+   * @param  {Object}   rpc     RPC Object
+   * @param  {String}   rpc.method The method from the last payload
+   * @param  {Object}   rpc.params Any input data the user provided
+   * @param  {Function} cb      Used to match up requests.
+   */
+  this.config = function(rpc, cb) {
+    // If rpc is null, we should send the user a menu of what he/she
+    // can do.
+    // Otherwise, we will try action the rpc method
+    if (!rpc)
+      return configHandlers.menu.call(this, cb);
+    else if (typeof configHandlers[rpc.method] === "function")
+      return configHandlers[rpc.method].call(this, rpc.params, cb);
+    else
+      return cb(true);
+  };
 	
-  app.on('client::up',function(){
-    // The client is now connected to the Ninja Platform
-    // Register a device
-    self.emit('register', new Device(self._opts, self._app.log));
+  app.on('client::up',function() {
+    self.emit('register', device);
+
+    process.nextTick(function() {
+      updateObservable.start();
+      device.start();
+    });
   });
-};
-
-/**
- * Called when a user prompts a configuration.
- * If `rpc` is null, the user is asking for a menu of actions
- * This menu should have rpc_methods attached to them
- *
- * @param  {Object}   rpc     RPC Object
- * @param  {String}   rpc.method The method from the last payload
- * @param  {Object}   rpc.params Any input data the user provided
- * @param  {Function} cb      Used to match up requests.
- */
-DuskTillDawn.prototype.config = function(rpc, cb) {
-  var self = this;
-
-  // If rpc is null, we should send the user a menu of what he/she
-  // can do.
-  // Otherwise, we will try action the rpc method
-  if (!rpc)
-    return configHandlers.menu.call(this, cb);
-  else if (typeof configHandlers[rpc.method] === "function")
-    return configHandlers[rpc.method].call(this, rpc.params, cb);
-  else
-    return cb(true);
 };
